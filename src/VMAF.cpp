5c1a5e08fc6c8979dd3fec2a87c9cac81f082269
Asd-g <65298684+Asd-g@users.noreply.github.com>
2021-06-28 02:39:00 +0300

    Change AviSynth API to C

--- src/VMAF.cpp
+++ src/VMAF.cpp
@@ -22,57 +22,50 @@
 #include <Windows.h>
 #include <filesystem>
 #include <mutex>
+#include <thread>
+#include <condition_variable>
 
-#include "avisynth.h"
+#include "avisynth_c.h"
 #include "libvmaf.h"
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
-class VMAF : public GenericVideoFilter
+struct VMAF
 {
-    PClip distorted_;
+    AVS_Clip* distorted;
+    const AVS_VideoInfo* vi;
     double vmafScore;
-    char* fmt, * logfmt, * pool_;
+    char* fmt, * logfmt, * pool;
     std::unique_ptr<char[]> modelPath, logPath;
-    bool ssim_, ms_ssim_, ci_;
+    bool ssim, ms_ssim, ci;
     std::thread* vmafThread;
 
-    void callVMAF() noexcept;
-
-public:
-    inline static PVideoFrame reference, main;
-    inline static bool frameSet, eof;
-    inline static float divisor;
-    inline static std::mutex mtx;
-    inline static std::condition_variable cond;
-    inline static int error;
-
-    VMAF(PClip _child, PClip distorted, int model, const char* log_path, int log_fmt, bool ssim, bool ms_ssim, int pool, bool ci, IScriptEnvironment* env);
-    PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
-    int __stdcall SetCacheHints(int cachehints, int frame_range)
-    {
-        return cachehints == CACHE_GET_MTMODE ? MT_SERIALIZED : 0;
-    }
-
-    ~VMAF();
+    AVS_VideoFrame* reference,* main;
+    bool frameSet, eof;
+    float divisor;
+    std::mutex mtx;
+    std::condition_variable cond;
+    int error;
 };
 
 template<typename T>
-static int readFrame(float* __restrict refData, float* __restrict mainData, float* __restrict tempData, const int strideByte, void* userData) noexcept
+int readFrame(float* __restrict refData, float* __restrict mainData, float* __restrict tempData, const int strideByte, void* userData) noexcept
 {
-    std::unique_lock<std::mutex> lck{ VMAF::mtx };
-    while (!VMAF::frameSet && !VMAF::eof)
-        VMAF::cond.wait(lck);
+    VMAF* const __restrict d = static_cast<VMAF*>(userData);
 
-    if (VMAF::frameSet)
+    std::unique_lock<std::mutex> lck{ d->mtx };
+    while (!d->frameSet && !d->eof)
+        d->cond.wait(lck);
+
+    if (d->frameSet)
     {
         const int pixel_size = sizeof(T);
-        const int srcStride = VMAF::reference->GetPitch() / pixel_size;
+        const int srcStride = avs_get_pitch(d->reference) / pixel_size;
         const int dstStride = strideByte / sizeof(float);
-        const int height = VMAF::reference->GetHeight();
-        const int width = VMAF::reference->GetRowSize() / pixel_size;
-        const T* refp = reinterpret_cast<const T*>(VMAF::reference->GetReadPtr());
-        const T* mainp = reinterpret_cast<const T*>(VMAF::main->GetReadPtr());
+        const int height = avs_get_height(d->reference);
+        const int width = avs_get_row_size(d->reference) / pixel_size;
+        const T* refp = reinterpret_cast<const T*>(avs_get_read_ptr(d->reference));
+        const T* mainp = reinterpret_cast<const T*>(avs_get_read_ptr(d->main));
 
         for (int y = 0; y < height; ++y)
         {
@@ -85,8 +78,8 @@ static int readFrame(float* __restrict refData, float* __restrict mainData, floa
                 }
                 else
                 {
-                    refData[x] = refp[x] * VMAF::divisor;
-                    mainData[x] = mainp[x] * VMAF::divisor;
+                    refData[x] = refp[x] * d->divisor;
+                    mainData[x] = mainp[x] * d->divisor;
                 }
             }
 
@@ -97,151 +90,168 @@ static int readFrame(float* __restrict refData, float* __restrict mainData, floa
         }
     }
 
-    const bool ret = !VMAF::frameSet;
+    const bool ret = !d->frameSet;
 
-    VMAF::reference = nullptr;
-    VMAF::main = nullptr;
-    VMAF::frameSet = false;
+    d->reference = nullptr;
+    d->main = nullptr;
+    d->frameSet = false;
 
-    VMAF::cond.notify_one();
+    d->cond.notify_one();
 
     return ret ? 2 : 0;
 }
 
-void VMAF::callVMAF() noexcept
+void callVMAF(VMAF* const __restrict d) noexcept
 {
-    if (vi.ComponentSize() == 1)
-        VMAF::error = compute_vmaf(&vmafScore, fmt, vi.width, vi.height, readFrame<uint8_t>, 0, modelPath.get(), logPath.get(), logfmt, 0, 0, 0, 0, 0, ssim_, ms_ssim_, pool_, 0, 1, ci_);
+    if (avs_component_size(d->vi) == 1)
+        d->error = compute_vmaf(&d->vmafScore, d->fmt, d->vi->width, d->vi->height, readFrame<uint8_t>, d, d->modelPath.get(), d->logPath.get(), d->logfmt, 0, 0, 0, 0, 0, d->ssim, d->ms_ssim, d->pool, 0, 1, d->ci);
     else
-        VMAF::error = compute_vmaf(&vmafScore, fmt, vi.width, vi.height, readFrame<uint16_t>, 0, modelPath.get(), logPath.get(), logfmt, 0, 0, 0, 0, 0, ssim_, ms_ssim_, pool_, 0, 1, ci_);
+        d->error = compute_vmaf(&d->vmafScore, d->fmt, d->vi->width, d->vi->height, readFrame<uint16_t>, d, d->modelPath.get(), d->logPath.get(), d->logfmt, 0, 0, 0, 0, 0, d->ssim, d->ms_ssim, d->pool, 0, 1, d->ci);
 
-    if (VMAF::error)
+    if (d->error)
     {
-        VMAF::mtx.lock();
-        VMAF::cond.notify_one();
-        VMAF::mtx.unlock();
+        d->mtx.lock();
+        d->cond.notify_one();
+        d->mtx.unlock();
     }
 }
 
-VMAF::VMAF(PClip _child, PClip distorted, int model, const char* log_path, int log_fmt, bool ssim, bool ms_ssim, int pool, bool ci, IScriptEnvironment* env)
-    : GenericVideoFilter(_child), distorted_(distorted), ssim_(ssim), ms_ssim_(ms_ssim), ci_(ci)
+AVS_VideoFrame* AVSC_CC vmaf_get_frame(AVS_FilterInfo* fi, int n)
 {
-    if (vi.BitsPerComponent() > 16)
-        env->ThrowError("VMAF: only 8..16-bit input supported.");
-    if (vi.IsRGB())
-        env->ThrowError("VMAF: RGB color family is not supported.");
-    if (!vi.IsPlanar())
-        env->ThrowError("VMAF: only planar format supported.");
+    VMAF* d = reinterpret_cast<VMAF*>(fi->user_data);
 
-    const VideoInfo& vi1 = distorted->GetVideoInfo();
-    if (!vi.IsSameColorspace(vi1))
-        env->ThrowError("VMAF: both clips must be the same format.");
-    if (vi.width != vi1.width || vi.height != vi1.height)
-        env->ThrowError("VMAF: both clips must have the same dimensions.");
-    if (vi.num_frames != vi1.num_frames)
-        env->ThrowError("VMAF: both clips' number of frames don't match.");
+    std::unique_lock<std::mutex> lck{ d->mtx };
+    while (d->frameSet && !d->error)
+        d->cond.wait(lck);
 
-    if (model < 0 || model > 1)
-        env->ThrowError("VMAF: model must be 0 or 1.");
-    if (log_fmt < 0 || log_fmt > 2)
-        env->ThrowError("VMAF: log_fmt must be 0, 1, or 2.");
-    if (pool < 0 || pool > 2)
-        env->ThrowError("VMAF: pool must be 0, 1, or 2.");
+    AVS_VideoFrame* ref = avs_get_frame(fi->child, n);
+    AVS_VideoFrame* dist = avs_get_frame(d->distorted, n);
+    d->reference = ref;
+    d->main = dist;
+    d->frameSet = true;
 
-    fmt = const_cast<char*>("yuv420p");
+    d->cond.notify_one();
+
+    return ref;
+}
+
+void AVSC_CC free_vmaf(AVS_FilterInfo* fi)
+{
+    VMAF* d = reinterpret_cast<VMAF*>(fi->user_data);
+
+    d->mtx.lock();
+    d->eof = true;
+    d->cond.notify_one();
+    d->mtx.unlock();
+
+    d->vmafThread->join();
+
+    delete d->vmafThread;
+
+    avs_release_clip(d->distorted);
+    delete d;
+}
+
+AVS_Value AVSC_CC Create_VMAF(AVS_ScriptEnvironment* env, AVS_Value args, void* param)
+{
+    AVS_Value v;
+    AVS_FilterInfo* fi;
+
+    AVS_Clip* clip = avs_new_c_filter(env, &fi, avs_array_elt(args, 0), 1);
+
+    VMAF* params = new VMAF();
+    params->vi = &fi->vi;
+
+    params->distorted = avs_take_clip(avs_array_elt(args, 1), env);
+    const int model = (avs_defined(avs_array_elt(args, 2))) ? avs_as_int(avs_array_elt(args, 2)) : 0;
+    const char* log_path = avs_as_string(avs_array_elt(args, 3));
+    const int log_fmt = (avs_defined(avs_array_elt(args, 4))) ? avs_as_int(avs_array_elt(args, 4)) : 0;
+    params->ssim = (avs_defined(avs_array_elt(args, 5))) ? avs_as_bool(avs_array_elt(args, 5)) : false;
+    params->ms_ssim = (avs_defined(avs_array_elt(args, 6))) ? avs_as_bool(avs_array_elt(args, 6)) : false;
+    const int pool = (avs_defined(avs_array_elt(args, 7))) ? avs_as_int(avs_array_elt(args, 7)) : 1;
+    params->ci = (avs_defined(avs_array_elt(args, 8))) ? avs_as_bool(avs_array_elt(args, 8)) : false;
+
+    v = avs_void;
+    const int bits = avs_bits_per_component(&fi->vi);
+
+    if (!avs_defined(v) && bits > 16)
+        v = avs_new_value_error("VMAF: only 8..16-bit input supported.");
+    if (!avs_defined(v) && avs_is_rgb(&fi->vi))
+        v = avs_new_value_error("VMAF: RGB color family is not supported.");
+    if (!avs_defined(v) && !avs_is_planar(&fi->vi))
+        v = avs_new_value_error("VMAF: only planar format supported.");
+
+    const AVS_VideoInfo* vi1 = avs_get_video_info(params->distorted);
+
+    if (!avs_defined(v) && !avs_is_same_colorspace(&fi->vi, vi1))
+        v = avs_new_value_error("VMAF: both clips must be the same format.");
+    if (!avs_defined(v) && fi->vi.width != vi1->width || fi->vi.height != vi1->height)
+        v = avs_new_value_error("VMAF: both clips must have the same dimensions.");
+    if (!avs_defined(v) && fi->vi.num_frames != vi1->num_frames)
+        v = avs_new_value_error("VMAF: both clips' number of frames don't match.");
+
+    if (!avs_defined(v) && (model < 0 || model > 1))
+        v = avs_new_value_error("VMAF: model must be 0 or 1.");
+    if (!avs_defined(v) && (log_fmt < 0 || log_fmt > 2))
+        v = avs_new_value_error("VMAF: log_fmt must be 0, 1, or 2.");
+    if (!avs_defined(v) && (pool < 0 || pool > 2))
+        v = avs_new_value_error("VMAF: pool must be 0, 1, or 2.");
+
+    params->fmt = const_cast<char*>("yuv420p");
 
     std::vector<wchar_t> charBuffer;
-    do
-    {
+    do {
         charBuffer.resize(512);
     } while (GetModuleFileNameW((HINSTANCE)&__ImageBase, charBuffer.data(), 512) == 512);
     std::filesystem::path pluginPath(charBuffer.data());
     std::string path = pluginPath.remove_filename().string();
 
     if (model == 0)
-        path += ci_ ? "model\\vmaf_b_v0.6.3\\vmaf_b_v0.6.3.pkl" : "model\\vmaf_v0.6.1.pkl";
+        path += params->ci ? "model\\vmaf_b_v0.6.3\\vmaf_b_v0.6.3.pkl" : "model\\vmaf_v0.6.1.pkl";
     else
-        path += ci_ ? "model\\vmaf_4k_rb_v0.6.2\\vmaf_4k_rb_v0.6.2.pkl" : "model\\vmaf_4k_v0.6.1.pkl";
+        path += params->ci ? "model\\vmaf_4k_rb_v0.6.2\\vmaf_4k_rb_v0.6.2.pkl" : "model\\vmaf_4k_v0.6.1.pkl";
 
-    modelPath = std::make_unique<char[]>(path.length() + 1);
-    strcpy_s(modelPath.get(), path.size() + 1, path.c_str());
+    params->modelPath = std::make_unique<char[]>(path.length() + 1);
+    strcpy_s(params->modelPath.get(), path.size() + 1, path.c_str());
 
     if (log_path)
     {
-        logPath = std::make_unique<char[]>(*log_path);
-        strcpy_s(logPath.get(), strlen(log_path) + 1, log_path);
+        params->logPath = std::make_unique<char[]>(*log_path);
+        strcpy_s(params->logPath.get(), strlen(log_path) + 1, log_path);
     }
 
     switch (log_fmt)
     {
-        case 0: logfmt = const_cast<char*>("xml"); break;
-        case 1: logfmt = const_cast<char*>("json"); break;
-        default: logfmt = const_cast<char*>("csv"); break;
+        case 0: params->logfmt = const_cast<char*>("xml"); break;
+        case 1: params->logfmt = const_cast<char*>("json"); break;
+        default: params->logfmt = const_cast<char*>("csv"); break;
     }
 
     switch (pool)
     {
-        case 0: pool_ = const_cast<char*>("mean"); break;
-        case 1: pool_ = const_cast<char*>("harmonic_mean"); break;
-        default: pool_ = const_cast<char*>("min"); break;
+        case 0: params->pool = const_cast<char*>("mean"); break;
+        case 1: params->pool = const_cast<char*>("harmonic_mean"); break;
+        default: params->pool = const_cast<char*>("min"); break;
     }
 
-    divisor = 1.0f / (1 << (vi.BitsPerComponent() - 8));
+    params->divisor = 1.0f / (1 << (bits - 8));
 
-    vmafThread = new std::thread(&VMAF::callVMAF, this);
+    params->vmafThread = new std::thread(callVMAF, &*params);
+
+    if (!avs_defined(v))
+        v = avs_new_value_clip(clip);
+
+    fi->user_data = reinterpret_cast<void*>(params);
+    fi->get_frame = vmaf_get_frame;
+    fi->free_filter = free_vmaf;
+
+    avs_release_clip(clip);
+
+    return v;
 }
 
-VMAF::~VMAF()
+const char* AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment* env)
 {
-    mtx.lock();
-    eof = true;
-    cond.notify_one();
-    mtx.unlock();
-
-    vmafThread->join();
-
-    delete vmafThread;
-}
-
-PVideoFrame __stdcall VMAF::GetFrame(int n, IScriptEnvironment* env)
-{
-    std::unique_lock<std::mutex> lck{ mtx };
-    while (frameSet && !VMAF::error)
-        cond.wait(lck);
-
-    PVideoFrame ref = child->GetFrame(n, env);
-    PVideoFrame dist = distorted_->GetFrame(n, env);
-    reference = ref;
-    main = dist;
-    frameSet = true;
-
-    cond.notify_one();
-
-    return ref;
-}
-
-AVSValue __cdecl Create_VMAF(AVSValue args, void* user_data, IScriptEnvironment* env)
-{
-    return new VMAF(
-        args[0].AsClip(),
-        args[1].AsClip(),
-        args[2].AsInt(0),
-        args[3].AsString(),
-        args[4].AsInt(0),
-        args[5].AsBool(false),
-        args[6].AsBool(false),
-        args[7].AsInt(1),
-        args[8].AsBool(false),
-        env);
-}
-
-const AVS_Linkage* AVS_linkage;
-
-extern "C" __declspec(dllexport)
-const char* __stdcall AvisynthPluginInit3(IScriptEnvironment * env, const AVS_Linkage* const vectors)
-{
-    AVS_linkage = vectors;
-
-    env->AddFunction("VMAF", "cc[model]i[log_path]s[log_fmt]i[ssim]b[ms_ssim]b[pool]i[ci]b", Create_VMAF, 0);
+    avs_add_function(env, "VMAF", "cc[model]i[log_path]s[log_fmt]i[ssim]b[ms_ssim]b[pool]i[ci]b", Create_VMAF, 0);
     return "VMAF";
 }
