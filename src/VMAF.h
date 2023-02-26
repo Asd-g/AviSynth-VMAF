742ef6739ad4be04b0ce27470925287d3f0394ff
Asd-g <65298684+Asd-g@users.noreply.github.com>
2023-02-27 01:47:29 +0200

    LICENSE: update the year

--- src/VMAF.h
+++ src/VMAF.h
@@ -4,7 +4,7 @@
   MIT License
 
   Copyright (c) 2018-2019 HolyWu
-  Copyright (c) 2020 Asd-g
+  Copyright (c) 2020-2022 Asd-g
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
